76032692de6c6a9bac0fafca9f78e4a01baaa4b5
4Marsha1 <abhishekbharadwaz22@gmail.com>
2022-12-17 11:02:07 +0530

    Async JS #3

---
+++ Async JS/eventLoop.js
@@ -0,0 +1,10 @@
+
+
+
+// Youtube Link:
+let url = 'https://www.youtube.com/watch?v=exBgWAIeIeg&list=PLC3y8-rFHvwhuX4qGvFx-wPy_MEi6Jdp7&index=7&ab_channel=Codevolution'
+window.open(url, '_blank')
+
+// 55:35 
+// to 
+// 1:27:35
